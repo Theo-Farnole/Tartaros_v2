ba367614b7162dd2337877163cc09fa64d254183
TF <theo.farnole@gmail.com>
2021-03-02 16:10:14 +0100

    ADD EntityData

---
+++ Assets/Scripts/Game/Entities/EntityData.cs
@@ -0,0 +1,17 @@
+﻿namespace Tartaros.Entities
+{
+	using Sirenix.OdinInspector;
+	using Sirenix.Serialization;
+
+	public class EntityData : SerializedScriptableObject
+	{
+		#region Fields
+		[OdinSerialize]
+		private IEntityBehaviourData[] _behaviours = new IEntityBehaviourData[0];
+		#endregion Fields
+
+		#region Properties
+		public IEntityBehaviourData[] Behaviours => _behaviours;
+		#endregion Properties
+	}
+}
