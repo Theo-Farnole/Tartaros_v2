da47283885910ff57853f96add604a8c0a717e8f
TF <theo.farnole@gmail.com>
2021-03-02 15:38:11 +0100

    ADD data types: Bounds + Bounds2D

---
+++ Assets/Scripts/Utilities/Data_Types/Bounds.cs
@@ -0,0 +1,24 @@
+﻿namespace Tartaros.Utilities
+{
+	using UnityEngine;
+
+	[System.Serializable]
+	public struct Bounds
+	{
+		#region Fields
+		[SerializeField]
+		public int min;
+
+		[SerializeField]
+		public int max;
+		#endregion Fields
+
+		#region Properties
+		public Bounds(int min, int max)
+		{
+			this.min = min;
+			this.max = max;
+		} 
+		#endregion Properties
+	}
+}
---
+++ Assets/Scripts/Utilities/Data_Types/Bounds2D.cs
@@ -0,0 +1,38 @@
+﻿namespace Tartaros.Utilities
+{
+	using UnityEngine;
+
+	public class Bounds2D
+	{
+		#region Fields
+		[SerializeField]
+		public Bounds boundsY;
+
+		[SerializeField]
+		public Bounds boundsX;
+		#endregion Fields
+
+		#region Properties
+		public int MinX { get => boundsX.min; set => boundsX.min = value; }
+		public int MaxX { get => boundsX.max; set => boundsX.max = value; }
+
+		public int MinY { get => boundsY.min; set => boundsY.min = value; }
+		public int MaxY { get => boundsY.max; set => boundsY.max = value; }
+		#endregion Properties
+
+		#region Ctor
+		public Bounds2D(int minX, int maxX, int minY, int maxY)
+		{
+			boundsX = new Bounds(minX, maxX);
+			boundsY = new Bounds(minY, maxY);
+		}
+
+		public Bounds2D(Bounds boundsX, Bounds boundsY)
+		{
+			this.boundsX = boundsX;
+			this.boundsY = boundsY;
+		}
+		#endregion Ctor
+	}
+
+}
