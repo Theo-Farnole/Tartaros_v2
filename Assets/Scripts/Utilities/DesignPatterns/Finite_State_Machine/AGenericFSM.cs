758cf2d02eb864206b3310d3bd021fb03834c5ab
TF <theo.farnole@gmail.com>
2021-03-02 14:41:21 +0100

    ADD utilities finite state machine

---
+++ Assets/Scripts/Utilities/DesignPatterns/Finite_State_Machine/AGenericFSM.cs
@@ -0,0 +1,44 @@
+﻿namespace Tartaros.Utilities
+{
+	public abstract class AGenericFSM<T>
+	{
+		#region Fields
+		private AState<T> _currentState = null;		
+		#endregion Fields
+
+		#region Properties
+		public AState<T> CurrentState
+		{
+			get => _currentState;
+
+			set
+			{
+				if (_currentState != null)
+				{
+					_currentState.OnStateExit();
+				}
+
+				_currentState = value;
+
+				if (_currentState != null)
+				{
+					_currentState.OnStateEnter();
+				}
+			}
+		}
+		#endregion Properties
+
+		#region Methods
+		/// <summary>
+		/// Must be called by a MonoBehaviour in Update
+		/// </summary>
+		public void OnUpdate()
+		{
+			if (_currentState != null)
+			{
+				_currentState.OnUpdate();
+			}
+		}
+		#endregion Methods
+	}
+}
