4e8f8f1c1c9afe9c4669edd8748ec4de5bf010fd
TF <theo.farnole@gmail.com>
2021-03-03 14:22:02 +0100

    ADD GizmosWireSphere

---
+++ Assets/Scripts/Utilities/Components/Gizmos/GizmosWireSphere.cs
@@ -0,0 +1,39 @@
+﻿namespace Tartaros.Utilities
+{
+	using UnityEngine;
+
+	public class GizmosWireSphere : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField]
+		private Color _sphereColor = Color.red;
+
+		[SerializeField]
+		private float _sphereRadius = 1f;
+
+		[SerializeField]
+		private bool _drawOnSelectedOnly = false;
+		#endregion Fields
+
+		#region Methods
+		private void OnDrawGizmos()
+		{
+			DrawSphere();
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			if (_drawOnSelectedOnly == true)
+			{
+				DrawSphere();
+			}
+		}
+
+		void DrawSphere()
+		{
+			Gizmos.color = _sphereColor;
+			Gizmos.DrawWireSphere(transform.position, _sphereRadius);
+		}
+		#endregion Methods
+	}
+}
