fbfd6d5b24c5e22f45ccc6517c61ef7631354315
TF <theo.farnole@gmail.com>
2021-03-03 11:17:28 +0100

    ADD collection extensions: IsEmpty() and ContainsElement()

---
+++ Assets/Scripts/Utilities/Extensions/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Tartaros
+{
+	using System.Collections.Generic;
+
+	public static class CollectionExtensions
+	{
+		public static bool IsEmpty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
+
+		public static bool ContainsElement<T>(this ICollection<T> collection)
+		{
+			return collection.Count > 0;
+		}
+
+		public static bool IsEmpty<T>(this Queue<T> queue) => (queue as ICollection<T>).IsEmpty();
+
+		public static bool ContainsElement<T>(this Queue<T> queue) => (queue as ICollection<T>).ContainsElement();
+	}
+}
