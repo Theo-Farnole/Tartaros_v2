ce8cdfe0f82995557c6bb512a2c99834c30da77a
TF <theo.farnole@gmail.com>
2021-03-05 10:10:05 +0100

    ADD getting behaviours methods in EntityData

--- Assets/Scripts/Game/Entities/EntityData.cs
+++ Assets/Scripts/Game/Entities/EntityData.cs
@@ -2,6 +2,7 @@
 {
 	using Sirenix.OdinInspector;
 	using Sirenix.Serialization;
+	using System.Linq;
 
 	public class EntityData : SerializedScriptableObject
 	{
@@ -13,5 +14,48 @@
 		#region Properties
 		public IEntityBehaviourData[] Behaviours => _behaviours;
 		#endregion Properties
+
+		#region Methods
+		public T GetBehaviour<T>() where T : class, IEntityBehaviourData
+		{
+			foreach (IEntityBehaviourData behaviour in _behaviours)
+			{
+				if (behaviour is T castedBehaviour)
+				{
+					return castedBehaviour;
+				}
+			}
+
+			return null;
+		}
+
+		public bool HasBehaviour<T>() where T : class, IEntityBehaviourData
+		{
+			foreach (IEntityBehaviourData behaviour in _behaviours)
+			{
+				if (behaviour is T)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public bool TryGetBehaviour<T>(out T behaviour) where T : class, IEntityBehaviourData
+		{
+			foreach (IEntityBehaviourData bhv in _behaviours)
+			{
+				if (bhv is T castedBehaviour)
+				{
+					behaviour = castedBehaviour;
+					return true;
+				}
+			}
+
+			behaviour = null;
+			return false;
+		}
+		#endregion Methods
 	}
 }
