8986a04569df8e59facec2ccc25c6d8d7ac702de
Dimitry <xxhellfire69xx@gmail.com>
2021-03-09 16:25:49 +0100

    ADD GamemodeFSM & AState

---
+++ Assets/Scripts/Game/GameMode/AGameState.cs
@@ -0,0 +1,31 @@
+﻿namespace Tartaros.Gamemode
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Tartaros.Utilities;
+
+    public abstract class AGameState : AState<GamemodeManager>
+    {
+        public AGameState(GamemodeManager stateOwner) : base(stateOwner)
+        {
+
+        }
+
+        public override void OnStateEnter()
+        {
+            base.OnStateEnter();
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+        }
+
+        public override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
--- Assets/Scripts/Game/GameMode/GamemodeManager.cs
+++ Assets/Scripts/Game/GameMode/GamemodeManager.cs
@@ -5,7 +5,6 @@
     using UnityEngine;
     using Tartaros.Gamemode;
 
-
     public class GamemodeManager : MonoBehaviour
     {
      
