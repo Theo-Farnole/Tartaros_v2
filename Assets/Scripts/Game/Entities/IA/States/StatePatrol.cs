306bd81a8e99f44f56bc0cdb873bd5b2a765ab1e
Dimitry <xxhellfire69xx@gmail.com>
2021-03-09 13:51:13 +0100

    ADD State Idle & Patrol

--- Assets/Scripts/Game/Entities/IA/States/StatePatrol.cs
+++ Assets/Scripts/Game/Entities/IA/States/StatePatrol.cs
@@ -5,19 +5,66 @@
 	using UnityEngine;
 	using Tartaros.Utilities;
 	using Tartaros.Entities;
+    using System;
 
-	public class StatePatrol : AEntityState
+    public class StatePatrol : AEntityState
 	{
 		private readonly Vector3[] _targetPoints;
+		private readonly EntityMovement _entityMovement = null;
+		private int _currentIndex = 0;
+		private int _maxIndex = 0;
 
 		public StatePatrol(Entity stateOwner, Vector3[] targetPoints) : base(stateOwner)
 		{
 			_targetPoints = targetPoints;
+			_entityMovement = stateOwner.GetComponent<EntityMovement>();
 		}
 
-		public override void OnUpdate()
+        public override void OnStateEnter()
+        {
+            base.OnStateEnter();
+
+			MoveToTargetPoint(0);
+
+			_entityMovement.DestinationReached -= DestinationReached;
+			_entityMovement.DestinationReached += DestinationReached;
+		}
+
+        public override void OnUpdate()
 		{
+
 			throw new System.NotImplementedException();
 		}
+
+		private void MoveToTargetPoint(int index)
+        {
+			if (_entityMovement.CanMoveToPoint(_targetPoints[index]))
+			{
+				_maxIndex = _targetPoints.Length;
+				_entityMovement.MoveToPoint(_targetPoints[index]);
+			}
+		}
+
+		private void ChangeTargetPoint()
+        {
+			if(_currentIndex < _maxIndex)
+            {
+				_currentIndex++;
+            }
+            else
+            {
+				_currentIndex = 0;
+            }
+
+			MoveToTargetPoint(_currentIndex);
+		}
+
+
+
+        private void DestinationReached(object sender, EntityMovement.DestinationReachedArgs e)
+        {
+			ChangeTargetPoint();
+        }
+
 	}
 }
