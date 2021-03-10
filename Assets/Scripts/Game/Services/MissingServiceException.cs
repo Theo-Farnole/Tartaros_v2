f603d12c9eee6b9ac7ef625fa9cab5c709166af9
TF <theo.farnole@gmail.com>
2021-03-10 10:59:54 +0100

    ADD exception when a service is missing

---
+++ Assets/Scripts/Game/Services/MissingServiceException.cs
@@ -0,0 +1,12 @@
+﻿namespace Tartaros.ServicesLocator
+{
+	using System;
+
+	public class MissingServiceException<TMissingService> : Exception
+	{
+		public MissingServiceException()
+			: base(string.Format("The service {0} is missing. Please, register it in Services.", typeof(TMissingService).Name))
+		{
+		}
+	}
+}
