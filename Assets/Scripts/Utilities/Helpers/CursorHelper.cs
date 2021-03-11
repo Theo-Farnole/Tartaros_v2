9de56f8ea0d9d4bb72e0fe1f23578f15b55973dd
TF <theo.farnole@gmail.com>
2021-03-11 15:33:05 +0100

    ADD CursorHelper

---
+++ Assets/Scripts/Utilities/Helpers/CursorHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Tartaros.Utilities
+{
+	using UnityEngine;
+	using UnityEngine.InputSystem;
+
+	public static class CursorHelper
+	{
+		public static Vector2 CursorPosition => Mouse.current.position.ReadValue();
+
+		public static GameObject GetGameObjectUnderCursor()
+		{			
+			Ray ray = Camera.main.ScreenPointToRay(CursorPosition);
+
+			if (Physics.Raycast(ray, out RaycastHit hit))
+			{
+				return hit.transform.gameObject;
+			}
+
+			return null;
+		}
+	}
+}
