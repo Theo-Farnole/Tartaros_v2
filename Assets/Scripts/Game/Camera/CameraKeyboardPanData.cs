9964106d06574b4d27c9e8f9701e363cf7834e3c
Dimitry <xxhellfire69xx@gmail.com>
2021-03-11 12:04:32 +0100

    RETAKE cameraController

--- Assets/Scripts/Game/Camera/CameraKeyboardPanData.cs
+++ Assets/Scripts/Game/Camera/CameraKeyboardPanData.cs
@@ -4,8 +4,17 @@
     using System.Collections.Generic;
     using UnityEngine;
 
+    [System.Serializable]
     public class CameraKeyboardPanData
     {
+        [SerializeField]
         float _speed = 1;
+
+        public CameraKeyboardPanData(float speed)
+        {
+            _speed = speed;
+        }
+
+        public float Speed => _speed;
     }
 }
