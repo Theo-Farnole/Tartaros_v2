c7202ed05d1e817db2b1ccf276a8a276298ff559
TF <theo.farnole@gmail.com>
2021-03-12 10:12:43 +0100

    ADD component to set entity's team at startup

---
+++ Assets/Scripts/Game/Entities/Debug_EntitySetTeam.cs
@@ -0,0 +1,33 @@
+﻿namespace Tartaros.Entities
+{
+	using Sirenix.OdinInspector;
+	using UnityEngine;
+
+
+	public class Debug_EntitySetTeam : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField]
+		[SuffixLabel("self is null")]
+		private Entity _entity = null;
+
+		[SerializeField]
+		private Team _teamSetOnStart = Team.Player;
+		#endregion Fields
+
+		#region Methods
+		void Awake()
+		{
+			if (_entity == null)
+			{
+				_entity = GetComponent<Entity>();
+			}
+		}
+
+		void Start()
+		{
+			_entity.Initialize(_teamSetOnStart, _entity.EntityType);
+		}
+		#endregion Methods
+	}
+}
