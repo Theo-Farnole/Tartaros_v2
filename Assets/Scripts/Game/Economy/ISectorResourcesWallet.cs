f05b0754c6e823b3b936e043918602c7831bbdce
Dimitry <xxhellfire69xx@gmail.com>
2021-03-15 16:30:49 +0100

    ADD Eco setup

--- Assets/Scripts/Game/Economy/ISectorResourcesWallet.cs
+++ Assets/Scripts/Game/Economy/ISectorResourcesWallet.cs
@@ -7,5 +7,7 @@
         int GetAmount(SectorRessourceType ressource);
         void AddAmount(SectorRessourceType ressource, int amount);
         void RemoveAmount(SectorRessourceType ressource, int amount);
+        bool CanBuy(Price price);
+        void Buy(Price price);
     }
 }
---
+++ Assets/Scripts/Game/Economy/SectorResourcesWallet.cs
@@ -0,0 +1,33 @@
+﻿namespace Tartaros.Economy
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public class SectorResourcesWallet : ISectorResourcesWallet
+    {
+        bool ISectorResourcesWallet.CanBuy(Price price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ISectorResourcesWallet.AddAmount(SectorRessourceType ressource, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ISectorResourcesWallet.Buy(Price price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        int ISectorResourcesWallet.GetAmount(SectorRessourceType ressource)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ISectorResourcesWallet.RemoveAmount(SectorRessourceType ressource, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
