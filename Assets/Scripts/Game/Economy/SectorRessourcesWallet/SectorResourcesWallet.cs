2eb8c34eaaf1d326f0f5fa6d1024f3e6be8a5720
TF <theo.farnole@gmail.com>
2021-03-16 16:35:13 +0100

    ADD shorthand to write a SectorResourcesWallet with zero amount

--- Assets/Scripts/Game/Economy/SectorRessourcesWallet/SectorResourcesWallet.cs
+++ Assets/Scripts/Game/Economy/SectorRessourcesWallet/SectorResourcesWallet.cs
@@ -1,60 +1,80 @@
 ﻿namespace Tartaros.Economy
 {
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using Tartaros.Economy;
-    using System.Linq;
-    using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Tartaros.Economy;
+	using System.Linq;
+	using System;
 
-    [System.Serializable]
-    public class SectorResourcesWallet : ISectorResourcesWallet
-    {
-        [SerializeField]
-        private Dictionary<SectorRessourceType, int> _ressourceAmount;
+	[System.Serializable]
+	public class SectorResourcesWallet : ISectorResourcesWallet
+	{
+		#region Fields
+		[SerializeField]
+		private Dictionary<SectorRessourceType, int> _ressourceAmount;
 
-        public SectorResourcesWallet(SectorResourcesWallet walletToCopy)
-        {
-            _ressourceAmount = walletToCopy._ressourceAmount.ToDictionary(entry => entry.Key, entry => entry.Value);
-        }
+		public static readonly SectorResourcesWallet Zero = new SectorResourcesWallet(
+			new Dictionary<SectorRessourceType, int>()
+			{
+				{ SectorRessourceType.Food, 0 },
+				{ SectorRessourceType.Iron, 0 },
+				{ SectorRessourceType.Stone, 0 }
+			}
+		);
+		#endregion Fields
 
-        bool ISectorResourcesWallet.CanBuy(Price price)
-        {
-            return price.Amount <= _ressourceAmount[price.RessourceType];
-        }
+		#region Ctor
+		public SectorResourcesWallet(SectorResourcesWallet walletToCopy)
+		{
+			_ressourceAmount = walletToCopy._ressourceAmount.ToDictionary(entry => entry.Key, entry => entry.Value);
+		}
 
-        void ISectorResourcesWallet.AddAmount(SectorRessourceType ressource, int amount)
-        {
-            _ressourceAmount[ressource] += amount;
-        }
+		public SectorResourcesWallet(Dictionary<SectorRessourceType, int> ressourceAmount)
+		{
+			_ressourceAmount = ressourceAmount;
+		}
+		#endregion
 
-        void ISectorResourcesWallet.Buy(Price price)
-        {
-            (this as ISectorResourcesWallet).RemoveAmount(price.RessourceType, price.Amount);
-        }
+		#region Methods
+		bool ISectorResourcesWallet.CanBuy(Price price)
+		{
+			return price.Amount <= _ressourceAmount[price.RessourceType];
+		}
 
-        int ISectorResourcesWallet.GetAmount(SectorRessourceType ressource)
-        {
-            return _ressourceAmount[ressource];
-        }
+		void ISectorResourcesWallet.AddAmount(SectorRessourceType ressource, int amount)
+		{
+			_ressourceAmount[ressource] += amount;
+		}
 
-        void ISectorResourcesWallet.RemoveAmount(SectorRessourceType ressource, int amount)
-        {
-            if (_ressourceAmount[ressource] - amount > 0)
-            {
-                _ressourceAmount[ressource] -= amount;
-                Debug.Log(_ressourceAmount[ressource]);
-            }
-            else
-            {
-                Debug.LogError("wallet can't be under 0");
-                _ressourceAmount[ressource] = 0;
-            }
-        }
+		void ISectorResourcesWallet.Buy(Price price)
+		{
+			(this as ISectorResourcesWallet).RemoveAmount(price.RessourceType, price.Amount);
+		}
 
-        object ICloneable.Clone()
-        {
-            return new SectorResourcesWallet(this);
-        }
-    }
+		int ISectorResourcesWallet.GetAmount(SectorRessourceType ressource)
+		{
+			return _ressourceAmount[ressource];
+		}
+
+		void ISectorResourcesWallet.RemoveAmount(SectorRessourceType ressource, int amount)
+		{
+			if (_ressourceAmount[ressource] - amount > 0)
+			{
+				_ressourceAmount[ressource] -= amount;
+				Debug.Log(_ressourceAmount[ressource]);
+			}
+			else
+			{
+				Debug.LogError("wallet can't be under 0");
+				_ressourceAmount[ressource] = 0;
+			}
+		}
+
+		object ICloneable.Clone()
+		{
+			return new SectorResourcesWallet(this);
+		}
+		#endregion Methods
+	}
 }
