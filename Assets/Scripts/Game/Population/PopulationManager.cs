83a52e856534570a063ee1d49326fa7f766ef51b
TF <theo.farnole@gmail.com>
2021-03-16 13:57:12 +0100

    RETAKE some scripts while Dimitry's review (bis)

--- Assets/Scripts/Game/Population/PopulationManager.cs
+++ Assets/Scripts/Game/Population/PopulationManager.cs
@@ -1,51 +1,78 @@
 ﻿namespace Tartaros.Population
 {
-    using UnityEngine;
-    using System.Collections;
-    public class PopulationManager : MonoBehaviour, IPopulationManager
-    {
-        private int _currentPopulation = 0;
-        private int _maxPopulation = 0;
+	using UnityEngine;
+	using System.Collections;
+	using Tartaros.ServicesLocator;
 
-        [SerializeField]
-        private PopulationManagerData _populationManagerData = null;
+	public class PopulationManager : MonoBehaviour, IPopulationManager
+	{
+		#region Fields
+		private int _currentPopulation = 0;
+		private int _maxPopulation = 0;
+
+		[SerializeField]
+		private PopulationManagerData _populationManagerData = null;
+		#endregion Fields
 
 
-        private void OnEnable()
-        {
-            _maxPopulation = _populationManagerData.StartingMaxPoupulation;
-        }
-        public bool CanSpawn(int popAmount)
-        {
-            return _currentPopulation + popAmount < _maxPopulation;
-        }
+		#region Methods
+		void Awake()
+		{
+			Services.Instance.RegisterService<IPopulationManager>(this);
+			_maxPopulation = _populationManagerData.StartingMaxPopulation;
+		}
 
-        public void AddCurrentPopulation(int popAmount)
-        {
-            if (_currentPopulation + popAmount < _maxPopulation)
-                _currentPopulation += popAmount;
-            else
-                Debug.LogError("_currentPop can't be supperior than _maxPop");
-        }
-        public void RemoveCurrentPopulation(int popAmount)
-        {
-            if (_currentPopulation + popAmount > 0)
-                _currentPopulation -= popAmount;
-            else
-                Debug.LogError("_currentPop can't be inferior than 0");
-        }
+		public bool CanSpawn(int popAmount)
+		{
+			return _currentPopulation + popAmount < _maxPopulation;
+		}
 
-        public void IncrementMaxPopulation(int popAmount)
-        {
-            _maxPopulation += popAmount;
-        }
+		public void AddCurrentPopulation(int popAmount)
+		{
+			if (popAmount < 0) throw new System.ArgumentException("PopAmount must be positive");
 
-        public void ReduceMaxPopulation(int popAmount)
-        {
-            if (_maxPopulation - popAmount > 0)
-                _maxPopulation -= popAmount;
-            else
-                Debug.LogError("_maxPop can't be inferrior than 0");
-        }
-    }
+			if (CanSpawn(popAmount) == true)
+			{
+				_currentPopulation += popAmount;
+			}
+			else
+			{
+				Debug.LogError("_currentPop can't be supperior than _maxPop");
+			}
+		}
+
+		public void RemoveCurrentPopulation(int popAmount)
+		{
+			if (popAmount < 0) throw new System.ArgumentException("PopAmount must be positive");
+
+			if (_currentPopulation - popAmount > 0)
+			{
+				_currentPopulation -= popAmount;
+			}
+			else
+			{
+				_currentPopulation = 0;
+				Debug.LogError("_currentPop can't be inferior than 0");
+			}
+		}
+
+		public void IncrementMaxPopulation(int popAmount)
+		{
+			_maxPopulation += popAmount;
+		}
+
+		public void ReduceMaxPopulation(int popAmount)
+		{
+			if (_maxPopulation - popAmount > 0)
+			{
+				_maxPopulation -= popAmount;
+			}
+			else
+			{
+				Debug.LogError("_maxPop can't be inferrior than 0");
+				_maxPopulation = 0;
+			}
+		}
+		#endregion Methods
+	}
 }
