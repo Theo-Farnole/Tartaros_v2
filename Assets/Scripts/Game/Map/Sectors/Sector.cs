2cac8709a2dce55f634df3b68667442ffb61ddfe
TF <theo.farnole@gmail.com>
2021-03-17 16:13:22 +0100

    ADD sector outline draw

--- Assets/Scripts/Game/Map/Sectors/Sector.cs
+++ Assets/Scripts/Game/Map/Sectors/Sector.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tartaros.Map
 {
+	using System.Collections.Generic;
 	using UnityEngine;
 
 	public class Sector : MonoBehaviour
@@ -12,7 +13,13 @@
 		private MeshCollider _collider = null;
 
 		private SectorData _sectorData = null;
+		private bool _isCaptured = false;
 		#endregion Fields
+
+		#region Properties
+		public SectorData SectorData => _sectorData;
+		public bool IsCaptured => _isCaptured;
+		#endregion Properties
 
 		#region Methods
 		public void Initialize(SectorData sectorData)
@@ -21,8 +28,31 @@
 
 			Mesh mesh = SectorMeshGenerator.GenerateMesh(_sectorData);
 
-			_meshFiltrer.mesh = mesh;
+			//_meshFiltrer.mesh = mesh;
 			_collider.sharedMesh = mesh;
+		}
+
+		public Vector3[] GetPointsWrappedSnappedToGround()
+		{
+			List<Vector3> sectorPointsSnapToGround = new List<Vector3>();
+
+			foreach (Vector3 sectorPoint in _sectorData.GetWorldPointsWrapped())
+			{
+				Ray ray = new Ray(sectorPoint + Vector3.up * 5, Vector3.down);
+
+				if (Physics.Raycast(ray, out RaycastHit hit))
+				{
+					sectorPointsSnapToGround.Add(hit.point);
+				}
+				else
+				{
+					sectorPointsSnapToGround.Add(sectorPoint);
+				}
+			}
+
+			Debug.LogFormat("{0} has generated an outline of {1} points.", name, sectorPointsSnapToGround.Count);
+
+			return sectorPointsSnapToGround.ToArray();
 		}
 		#endregion Methods
 	}
