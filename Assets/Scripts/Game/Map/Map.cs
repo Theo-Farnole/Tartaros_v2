3e754427ffbec1a4c627ef6ec74a4e76f1d417a6
TF <theo.farnole@gmail.com>
2021-03-17 14:40:08 +0100

    RETAKE refactor: extract sector setup logic into a MB

--- Assets/Scripts/Game/Map/Map.cs
+++ Assets/Scripts/Game/Map/Map.cs
@@ -21,25 +21,26 @@
 		#region Methods
 		private void Start()
 		{
-			GenerateSectorsMeshes();
+			SpawnSectors();
 		}
 
-		private void GenerateSectorsMeshes()
+		private void SpawnSectors()
 		{
 			for (int i = 0; i < _mapData.Sectors.Length; i++)
 			{
-				SectorData sector = _mapData.Sectors[i];
-				Mesh mesh = SectorMeshGenerator.GenerateMesh(sector);
-
 				// because polygon normal are forward, we must rotate to make them look up
 				GameObject sectorGameObject = Instantiate(_sectorPrefab, Vector3.zero, Quaternion.Euler(90, 0, 0));
 				sectorGameObject.name = string.Format("Sector {0}", i);
 
-				MeshFilter sectorMeshFilter = sectorGameObject.GetOrAddComponent<MeshFilter>();
-				sectorMeshFilter.mesh = mesh;
-
-				var meshCollider = sectorGameObject.GetOrAddComponent<MeshCollider>();
-				meshCollider.sharedMesh = mesh;
+				if (sectorGameObject.TryGetComponent(out Sector sector))
+				{
+					SectorData sectorData = _mapData.Sectors[i];
+					sector.Initialize(sectorData);
+				}
+				else
+				{
+					Debug.LogWarningFormat("Missing Sector component on prefab {0}.", _sectorPrefab.name);
+				}
 			}
 		}
 		#endregion Methods
---
+++ Assets/Scripts/Game/Map/Sectors/Sector.cs
@@ -0,0 +1,29 @@
+﻿namespace Tartaros.Map
+{
+	using UnityEngine;
+
+	public class Sector : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField]
+		private MeshFilter _meshFiltrer = null;
+
+		[SerializeField]
+		private MeshCollider _collider = null;
+
+		private SectorData _sectorData = null;
+		#endregion Fields
+
+		#region Methods
+		public void Initialize(SectorData sectorData)
+		{
+			_sectorData = sectorData;
+
+			Mesh mesh = SectorMeshGenerator.GenerateMesh(_sectorData);
+
+			_meshFiltrer.mesh = mesh;
+			_collider.sharedMesh = mesh;
+		}
+		#endregion Methods
+	}
+}
