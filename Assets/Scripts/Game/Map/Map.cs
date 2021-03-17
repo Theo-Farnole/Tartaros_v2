ab988ac63cc07f2252b7ba59a640909eede1339d
TF <theo.farnole@gmail.com>
2021-03-17 12:34:01 +0100

    ADD sector selection

--- Assets/Scripts/Game/Map/Map.cs
+++ Assets/Scripts/Game/Map/Map.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tartaros.Map
 {
+	using Sirenix.OdinInspector;
 	using UnityEngine;
 
 	public class Map : MonoBehaviour
@@ -7,6 +8,10 @@
 		#region Fields 
 		[SerializeField]
 		private MapData _mapData = null;
+
+		[SerializeField]
+		[AssetsOnly]
+		private GameObject _sectorPrefab = null;
 		#endregion Fields
 
 		#region Properties
@@ -24,16 +29,17 @@
 			for (int i = 0; i < _mapData.Sectors.Length; i++)
 			{
 				SectorData sector = _mapData.Sectors[i];
-				var mesh = SectorMeshGenerator.GenerateMesh(sector);
+				Mesh mesh = SectorMeshGenerator.GenerateMesh(sector);
 
-				string name = string.Format("Sector {0}", i);
+				// because polygon normal are forward, we must rotate to make them look up
+				GameObject sectorGameObject = Instantiate(_sectorPrefab, Vector3.zero, Quaternion.Euler(90, 0, 0));
+				sectorGameObject.name = string.Format("Sector {0}", i);
 
-				GameObject sectorGameObject = new GameObject(name, typeof(MeshRenderer));				
-				var sectorMeshFilter = sectorGameObject.AddComponent<MeshFilter>();
-
+				MeshFilter sectorMeshFilter = sectorGameObject.GetOrAddComponent<MeshFilter>();
 				sectorMeshFilter.mesh = mesh;
 
-				sectorGameObject.transform.Rotate(90, 0, 0);
+				var meshCollider = sectorGameObject.GetOrAddComponent<MeshCollider>();
+				meshCollider.sharedMesh = mesh;
 			}
 		}
 		#endregion Methods
