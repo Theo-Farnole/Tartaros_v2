be10076b300f6e080ff3ed79b7bb24999fbcf326
Dimitry <xxhellfire69xx@gmail.com>
2021-03-17 16:30:23 +0100

    ADD Wave SetUp & UnitSequence & WaveData

---
+++ Assets/Scripts/Game/Waves/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace Tartaros.Wave
+{
+    using System.Collections;
+    using UnityEngine;
+    using Tartaros;
+    public class SpawnPoint : MonoBehaviour, ISpawnPoint
+    {
+        private float _randomRadius = 0;
+        
+        SpawnPointIdentifier ISpawnPoint.Identifier => throw new System.NotImplementedException();
+
+        Vector3 ISpawnPoint.SpawnPoint => Random.insideUnitCircle.ToXZ() * _randomRadius + transform.position;
+    }
+}
---
+++ Assets/Scripts/Game/Waves/WavesSpawnerData.cs
@@ -0,0 +1,19 @@
+﻿namespace Tartaros.Wave
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class WavesSpawnerData : MonoBehaviour
+    {
+        private WaveData[] _waves;
+        private float _secondsBetweenWaves = 0;
+
+        void SpawnWave(int indexWave)
+        {
+            //TODO Dimi
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
