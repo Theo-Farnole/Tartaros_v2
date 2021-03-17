1eb4ec6786beb1ab3849459783c8dca08aea213e
TF <theo.farnole@gmail.com>
2021-03-17 15:34:00 +0100

    ADD .ToXZ Vector2 extension method

---
+++ Assets/Scripts/Utilities/Extensions/Vector2Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Tartaros
+{
+	using UnityEngine;
+
+	public static class Vector2Extensions
+	{
+		public static Vector3 ToXZ(this Vector2 v2)
+		{
+			return new Vector3(v2.x, 0, v2.y);
+		}
+	}
+}
