44f14005fb33f02f46fdfc5119a4a7990eb7c194
TF <theo.farnole@gmail.com>
2021-03-18 16:58:10 +0100

    ADD UI: player current resources amount

---
+++ Assets/Scripts/Game/UI/Wallet/PlayerResourceAmountLabel.cs
@@ -0,0 +1,74 @@
+﻿namespace Tartaros.UI
+{
+	using Sirenix.OdinInspector;
+	using Tartaros.Economy;
+	using Tartaros.ServicesLocator;
+	using TMPro;
+	using UnityEngine;
+
+	public class PlayerResourceAmountLabel : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField]
+		[SuffixLabel("self is null")]
+		private TextMeshProUGUI _amountLabel = null;
+
+		[SerializeField]
+		private SectorRessourceType _sectorRessourceType = SectorRessourceType.Food;
+
+		private IPlayerSectorResources _playerSectorResources = null;
+		#endregion Fields
+
+		#region Methods
+		private void Awake()
+		{
+			if (_amountLabel == null)
+			{
+				_amountLabel = GetComponent<TextMeshProUGUI>();
+			}
+		}
+
+		private void Start()
+		{
+			_playerSectorResources = Services.Instance.Get<IPlayerSectorResources>();
+			SubscribeToEventAmountChanged();
+			UpdateAmountLabel();
+		}
+
+		private void OnEnable()
+		{
+			SubscribeToEventAmountChanged();
+		}
+
+		private void OnDisable()
+		{
+			UnsubcribeToEventAmountChanged();
+		}
+
+		private void SubscribeToEventAmountChanged()
+		{
+			if (_playerSectorResources == null) return;
+
+			UnsubcribeToEventAmountChanged();
+			_playerSectorResources.AmountChanged += AmountChanged;
+		}
+
+		private void UnsubcribeToEventAmountChanged()
+		{
+			if (_playerSectorResources == null) return;
+
+			_playerSectorResources.AmountChanged -= AmountChanged;
+		}
+
+		private void AmountChanged(object sender, AmountChangedArgs e)
+		{
+			UpdateAmountLabel();
+		}
+
+		private void UpdateAmountLabel()
+		{
+			_amountLabel.text = _playerSectorResources.GetAmount(_sectorRessourceType).ToString();
+		}
+		#endregion Methods
+	}
+}
