8fbe2ecbb9c05d4bd472258550c7bab9adb21a87
TF <theo.farnole@gmail.com>
2021-03-19 14:04:12 +0100

    ADD WIP PlayerResourceIncomeLabel

--- Assets/Scripts/Game/Economy/Player_Income_Sectors_Ressources/PlayerIncomeSectorResources.cs
+++ Assets/Scripts/Game/Economy/Player_Income_Sectors_Ressources/PlayerIncomeSectorResources.cs
@@ -1,31 +1,37 @@
 ﻿namespace Tartaros.Economy
 {
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using Tartaros.Economy;
-    using ServicesLocator;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Tartaros.Economy;
+	using ServicesLocator;
+	using System;
 
-    public class PlayerIncomeSectorResources : MonoBehaviour, IPlayerIncomeSectorResources
-    {
-        private IPlayerSectorResources _playerSectorRessources = null;
-        [SerializeField]
-        private PlayerSectorResourcesData _data = null;
+	public class PlayerIncomeSectorResources : MonoBehaviour, IPlayerIncomeSectorResources
+	{
+		#region Fields
+		[SerializeField]
+		private PlayerSectorResourcesData _data = null;
 
-        
-        private void Awake()
-        {
-            Services.Instance.RegisterService<IPlayerIncomeSectorResources>(this);
-            _playerSectorRessources = Services.Instance.Get<IPlayerSectorResources>();
-        }
-        void IPlayerIncomeSectorResources.AddIncome(SectorRessourceType ressource, int amount)
-        {
-            _playerSectorRessources.AddAmount(ressource, amount);
-        }
+		private IPlayerSectorResources _playerSectorRessources = null;
+		#endregion Fields
 
-        void IPlayerIncomeSectorResources.RemoveIncome(SectorRessourceType ressource, int amount)
-        {
-            _playerSectorRessources.RemoveAmount(ressource, amount);
-        }
-    }
+		#region Properties
+		public event EventHandler<AmountChangedArgs> AmountChanged = null;
+		#endregion Properties
+
+		#region Methods
+		private void Awake()
+		{
+			Services.Instance.RegisterService<IPlayerIncomeSectorResources>(this);
+			_playerSectorRessources = Services.Instance.Get<IPlayerSectorResources>();
+		}
+
+		int ISectorResourcesWallet.GetAmount(SectorRessourceType ressource) => _playerSectorRessources.GetAmount(ressource);
+		void ISectorResourcesWallet.AddAmount(SectorRessourceType ressource, int amount) => _playerSectorRessources.AddAmount(ressource, amount);
+		void ISectorResourcesWallet.RemoveAmount(SectorRessourceType ressource, int amount) => _playerSectorRessources.RemoveAmount(ressource, amount);
+		bool ISectorResourcesWallet.CanBuy(ISectorResourcesWallet price) => _playerSectorRessources.CanBuy(price);
+		void ISectorResourcesWallet.Buy(ISectorResourcesWallet price) => _playerSectorRessources.Buy(price);
+		#endregion Methods
+	}
 }
