ca4085869bfbb0e310e4e47cb28f0e41436a17d6
TF <theo.farnole@gmail.com>
2021-03-23 14:06:41 +0100

    ADD extension method Clone

---
+++ Assets/Scripts/Utilities/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Tartaros
+{
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public static class DictionaryExtensions
+	{
+		public static Dictionary<TKey, TValue> Clone<TKey, TValue>(this Dictionary<TKey, TValue> toCloneDictionary)
+		{
+			Dictionary<TKey, TValue> clonedDictionary = toCloneDictionary.ToDictionary(entry => entry.Key, entry => entry.Value);
+			return clonedDictionary;
+		}
+	}
+}
