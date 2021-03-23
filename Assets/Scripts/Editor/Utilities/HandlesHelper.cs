8eaa9a58b4939f82678e2bc42d05ba9ec1ba1d19
TF <theo.farnole@gmail.com>
2021-03-23 10:07:51 +0100

    RETAKE MapEditor: change free move handle move visible

---
+++ Assets/Scripts/Editor/Utilities/HandlesHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Tartaros.Editor
+{
+	using System.Collections.Generic;
+	using UnityEditor;
+	using UnityEngine;
+
+	public static class HandlesHelper
+	{
+		private static readonly Color DEFAULT_COLOR = Color.white;
+		private static Stack<Color> _handlesColor = new Stack<Color>();
+
+		public static void PushColor(Color color)
+		{
+			_handlesColor.Push(color);
+
+			Handles.color = color;
+		}
+
+		public static void PopColor()
+		{
+			Handles.color = GetColor();
+
+			Color GetColor()
+			{
+				if (_handlesColor.Count > 1)
+				{
+					return _handlesColor.Pop();
+				}
+				else
+				{
+					return DEFAULT_COLOR;
+				}
+			}
+		}
+	}
+}
