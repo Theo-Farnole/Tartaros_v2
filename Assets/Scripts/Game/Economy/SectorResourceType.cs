20f29d46651c3b6987dc62b2176cf3a9cacb554e
TF <theo.farnole@gmail.com>
2021-03-23 10:43:38 +0100

    ADD FlagResourceToSector + ADD SectorResourceType extensions method

--- Assets/Scripts/Game/Economy/SectorResourceType.cs
+++ Assets/Scripts/Game/Economy/SectorResourceType.cs
@@ -1,9 +1,55 @@
 ﻿namespace Tartaros.Economy
 {
-    public enum SectorRessourceType
-    {
-        Iron,
-        Stone,
-        Food
-    }
+	using UnityEngine;
+
+	public enum SectorRessourceType
+	{
+		Iron,
+		Stone,
+		Food
+	}
+
+	public static class SectorRessourceTypeExtensions
+	{
+		public static Color GetDebugColor(this SectorRessourceType type)
+		{
+			switch (type)
+			{
+				case SectorRessourceType.Iron:
+					return Color.red;
+
+				case SectorRessourceType.Stone:
+					return Color.blue;
+
+				case SectorRessourceType.Food:
+					return Color.green;
+
+				default:
+					throw new System.NotImplementedException();
+			}
+		}
+
+		public static void DrawIcon(this SectorRessourceType type, Vector3 center)
+		{
+			Gizmos.DrawIcon(center, type.GetGizmosIconName());
+		}
+
+		private static string GetGizmosIconName(this SectorRessourceType type)
+		{
+			switch (type)
+			{
+				case SectorRessourceType.Iron:
+					return "iron.png";
+
+				case SectorRessourceType.Stone:
+					return "stone.png";
+					
+				case SectorRessourceType.Food:
+					return "food.png";
+
+				default:
+					throw new System.NotImplementedException();
+			}
+		}
+	}
 }
