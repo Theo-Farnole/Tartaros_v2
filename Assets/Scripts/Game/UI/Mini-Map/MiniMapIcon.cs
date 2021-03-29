66d08012335df60ba601d1ea1660585a8f830aed
Dimitry <xxhellfire69xx@gmail.com>
2021-03-29 16:27:13 +0200

    ADD WIP MiniMap iconPosition

---
+++ Assets/Scripts/Game/UI/Mini-Map/MiniMapIcon.cs
@@ -0,0 +1,38 @@
+﻿namespace Tartaros.UI.MiniMap
+{
+    using Tartaros.ServicesLocator;
+    using UnityEngine;
+
+    public class MiniMapIcon : MonoBehaviour, IMiniMapIcon
+    {
+        [SerializeField]
+        private Sprite _icon = null;
+
+        private MiniMap _miniMap = null;
+
+        Vector3 IMiniMapIcon.WorldPosition => transform.position;
+
+        Sprite IMiniMapIcon.Icon => _icon;
+
+        private void Awake()
+        {
+            _miniMap = Services.Instance.Get<MiniMap>();
+        }
+
+        void OnEnable()
+        {
+            _miniMap.AddIcon(this);
+        }
+
+        void OnDisable()
+        {
+            _miniMap.RemoveIcon(this);
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, 0.3f);
+        }
+    }
+}
