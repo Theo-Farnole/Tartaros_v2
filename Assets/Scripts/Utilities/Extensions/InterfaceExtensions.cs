1f654d5945843c73d662b746c997f99716eb04c2
TF <theo.farnole@gmail.com>
2021-03-31 14:06:19 +0200

    ADD extension method to check is an interface is destroyed

---
+++ Assets/Scripts/Utilities/Extensions/InterfaceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Tartaros
+{
+	using UnityEngine;
+
+	public static class InterfaceExtensions
+	{
+		public static bool IsInterfaceDestroyed<T>(this T @interface) where T : class
+		{
+			if (@interface is MonoBehaviour monoBehaviour)
+			{
+				return monoBehaviour == null;
+			}
+			else
+			{
+				throw new System.NotSupportedException("Interface {0} cannot be casted to a MonoBehaviour.".Format(@interface.GetType()));
+			}
+		}
+	}
+}
