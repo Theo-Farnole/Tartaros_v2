f92cb317269c5b3e39c51709408bbb76ee25c55c
TF <theo.farnole@gmail.com>
2021-03-31 16:14:00 +0200

    ADD UI: APanel + ADD WIP OneEntiySelectedPanel

---
+++ Assets/Scripts/Game/UI/Core/APanel.cs
@@ -0,0 +1,34 @@
+﻿namespace Tartaros.UI
+{
+	using UnityEngine;
+
+	[RequireComponent(typeof(Canvas))]
+	public class APanel : MonoBehaviour
+	{
+		#region Fields
+		private Canvas _canvas = null;
+		#endregion Fields
+
+		#region Properties
+		public bool IsShow => _canvas.enabled;
+		#endregion Properties
+
+		#region Methods
+
+		private void Awake()
+		{
+			_canvas = GetComponent<Canvas>();
+		}
+
+		public void Show()
+		{
+			_canvas.enabled = true;
+		}
+
+		public void Hide()
+		{
+			_canvas.enabled = false;
+		}
+		#endregion Methods
+	}
+}
---
+++ Assets/Scripts/Game/UI/Selections/OneEntitySelected/OneEntitySelectedPanel.cs
@@ -0,0 +1,49 @@
+﻿namespace Assets.Scripts.Game.UI.Selections.OneEntitySelected
+{
+	using Tartaros.Selection;
+	using Tartaros.ServicesLocator;
+	using Tartaros.UI;
+
+	public class OneEntitySelectedPanel : APanel
+	{
+		#region Fields
+		private ISelection _currentSelection = null;
+		#endregion Fields
+
+		#region Methods
+		private void Start()
+		{
+			_currentSelection = Services.Instance.Get<ISelection>();
+
+			_currentSelection.SelectionChanged -= SelectionChanged;
+			_currentSelection.SelectionChanged += SelectionChanged;
+		}
+
+		private void OnEnable()
+		{
+			if (_currentSelection != null)
+			{
+				_currentSelection.SelectionChanged -= SelectionChanged;
+				_currentSelection.SelectionChanged += SelectionChanged;
+			}
+		}
+
+		private void OnDisable()
+		{
+			_currentSelection.SelectionChanged -= SelectionChanged;
+		}
+
+		private void SelectionChanged(object sender, SelectionChangedArgs e)
+		{
+			if (_currentSelection.SelectedSelectables.Length == 1)
+			{
+				Show();
+			}
+			else
+			{
+				Hide();
+			}
+		} 
+		#endregion Methods
+	}
+}
