c1a21b75268eb02d026d10221984cff157fb8c2f
TF <theo.farnole@gmail.com>
2021-04-01 16:28:58 +0200

    FIX CanBuyWallet logic

---
+++ Assets/Scripts/Units_Tests/Editor_Tests/Economy/SectorResourcesWallet_CanBuyWallet.cs
@@ -0,0 +1,63 @@
+﻿namespace Tartaros.Tests.Economy
+{
+	using NUnit.Framework;
+	using Tartaros.Economy;
+	using UnityEngine.Assertions.Must;
+
+	public class SectorResourcesWallet_CanBuyWallet
+	{
+		[Test]
+		public void When_AllResourcesAreLesser_Should_ReturnTrue()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(0, 0, 0);
+
+			Assert.IsTrue(w1.CanBuyWallet(w2));
+		}
+
+		[Test]
+		public void When_AllResourcesAreEquals_Should_ReturnTrue()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(100, 100, 100);
+
+			Assert.IsTrue(w1.CanBuyWallet(w2));
+		}
+
+		[Test]
+		public void When_AllResourcesAreGreater_Should_ReturnFalse()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(200, 200, 200);
+
+			Assert.IsFalse(w1.CanBuyWallet(w2));
+		}
+
+		[Test]
+		public void When_OneResourceIsLesser_Should_ReturnTrue()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(0, 0, 0);
+
+			Assert.IsTrue(w1.CanBuyWallet(w2));
+		}
+
+		[Test]
+		public void When_OneResourceIsEquals_Should_ReturnTrue()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(100, 0, 0);
+
+			Assert.IsTrue(w1.CanBuyWallet(w2));
+		}
+
+		[Test]
+		public void When_OneResourceIsGreater_Should_ReturnFalse()
+		{
+			SectorResourcesWallet w1 = new SectorResourcesWallet(100, 100, 100);
+			SectorResourcesWallet w2 = new SectorResourcesWallet(106, 0, 0);
+
+			Assert.IsFalse(w1.CanBuyWallet(w2));
+		}
+	}
+}
