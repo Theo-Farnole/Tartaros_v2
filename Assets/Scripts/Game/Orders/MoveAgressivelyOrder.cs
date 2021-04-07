8da2d08057cac27d02e5ec2b4f43d660c5cc4a70
Dimitry <xxhellfire69xx@gmail.com>
2021-04-07 13:51:10 +0200

    ADD Order Move / Agrresive / patrol

---
+++ Assets/Scripts/Game/Orders/MoveAgressivelyOrder.cs
@@ -0,0 +1,49 @@
+﻿namespace Tartaros.Orders
+{
+	using System;
+	using System.Collections;
+	using Tartaros.Entities;
+	using Tartaros.Gamemode;
+	using Tartaros.OrderGiver;
+	using Tartaros.ServicesLocator;
+	using UnityEngine;
+
+	public class MoveAgressivelyOrder : Order
+	{
+		private static Sprite Icon => Services.Instance.Get<IconsDatabase>().Data.MoveAgressivelyIcon;
+		public MoveAgressivelyOrder(EntityMovement entityMovement) : base(Icon, Create(entityMovement))
+		{
+		}
+		private static Action Create(EntityMovement entityMovement)
+		{
+			return () =>
+			{
+				GamemodeManager gamemodeManager = Services.Instance.Get<GamemodeManager>();
+
+				CatchRightClickState state = new CatchRightClickState(gamemodeManager, OrderMoveToPositionUnderCursor(entityMovement));
+
+				gamemodeManager.SetState(state);
+			};
+		}
+
+		private static Action OrderMoveToPositionUnderCursor(EntityMovement entityMovement)
+		{
+			return () =>
+			{
+				var userErrorLogger = Services.Instance.Get<UserErrorsLogger>();
+				RaycastHit hit;
+				bool gameObjectUnderCursor = MouseHelper.GetHitUnderCursor(out hit);
+				Vector3 position = hit.point;
+
+				if (entityMovement.CanMoveToPoint(position))
+				{
+					entityMovement.GetComponent<IOrderMoveAggresivellyReceiver>().MoveAggressively(position);
+				}
+				else
+				{
+					userErrorLogger.Log("MovePoint isn't valable");
+				}
+			};
+		}
+	}
+}
