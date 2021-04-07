38c1ca560fc8731fc4a185689b90f242cdd2b978
TF <theo.farnole@gmail.com>
2021-04-07 11:19:58 +0200

    ADD start wave cheat

---
+++ Assets/Scripts/Game/Waves/EnemiesWavesManagerCheats.cs
@@ -0,0 +1,15 @@
+﻿namespace Tartaros.Wave
+{
+	using Tartaros.ServicesLocator;
+	using TF.CheatsGUI;
+	using UnityEngine;
+
+	public static class EnemiesWavesManagerCheats
+	{
+		[Cheat]
+		public static void StartWave()
+		{
+			GameObject.FindObjectOfType<EnemiesWavesManager>().StartNewWave();
+		}
+	}
+}
