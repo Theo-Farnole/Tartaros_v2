61a9afca6680f34a80b9218a66dbb3d2b44028c1
TF <theo.farnole@gmail.com>
2021-04-07 16:28:35 +0200

    ADD startup scene + ADD utilities: comment component, load scene on awake and scene reference

---
+++ Assets/Scripts/Common/Components/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Tartaros
+{
+	using Sirenix.OdinInspector;
+	using UnityEngine;
+
+	[InfoBox("An editor comment for developers | No impact on gameplay")]
+	public class Comment : MonoBehaviour
+	{
+		#region Fields
+		[TextArea(5, 1000)]
+		[SerializeField]
+		private string _comment = null; 
+		#endregion Fields
+	}
+}
---
+++ Assets/Scripts/Common/Components/LoadSceneOnAwake.cs
@@ -0,0 +1,29 @@
+﻿namespace Tartaros
+{
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public class LoadSceneOnAwake : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField]
+		private SceneReference _sceneToLoad = null;
+
+		[SerializeField]
+		private List<SceneReference> _additivesScenes = null;
+		#endregion Fields
+
+		#region Methods
+		private void Awake()
+		{
+			SceneManager.LoadScene(_sceneToLoad);
+
+			foreach (var additiveScene in _additivesScenes)
+			{
+				SceneManager.LoadScene(additiveScene, LoadSceneMode.Additive);
+			}
+		}
+		#endregion Methods
+	}
+}
