9d94a1b742be52e2304a0c4dfece39d9d09968f0
TF <theo.farnole@gmail.com>
2021-04-15 11:38:43 +0200

    ADD cheat to inflict damage to selected attackable

---
+++ Assets/Scripts/Game/Cheats/GameCheats.cs
@@ -0,0 +1,25 @@
+﻿namespace Tartaros.Cheats
+{
+	using Tartaros.Entities;
+	using Tartaros.Selection;
+	using Tartaros.ServicesLocator;
+	using TF.CheatsGUI;
+	using UnityEngine;
+
+	public static class GTameCheats
+	{
+		[Cheat]
+		public static void InflictDamageToSelected(int damage)
+		{
+			ISelection currentSelection = Services.Instance.Get<CurrentSelection>();
+
+			foreach (var selected in currentSelection.SelectedSelectables)
+			{
+				if (selected is MonoBehaviour selectedMonoBehaviour && selectedMonoBehaviour.TryGetComponent(out IAttackable attackable) == true)
+				{
+					attackable.TakeDamage(damage);
+				}
+			}
+		}
+	}
+}
