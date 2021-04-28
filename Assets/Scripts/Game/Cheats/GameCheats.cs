de5702fdb555afbe0601dc3be6556b3175e51c22
TF <theo.farnole@gmail.com>
2021-04-28 12:12:12 +0200

    ADD debug pause cheat: pause the game but allow the camera to move

--- Assets/Scripts/Game/Cheats/GameCheats.cs
+++ Assets/Scripts/Game/Cheats/GameCheats.cs
@@ -1,13 +1,18 @@
 ﻿namespace Tartaros.Cheats
 {
+	using Tartaros.CameraSystem;
 	using Tartaros.Entities;
 	using Tartaros.Selection;
 	using Tartaros.ServicesLocator;
 	using TF.CheatsGUI;
 	using UnityEngine;
 
-	public static class GTameCheats
+	public static class GameCheats
 	{
+		private static bool _isInDebugPause = false;
+
+		public static bool IsInDebugPause => _isInDebugPause;
+
 		[Cheat]
 		public static void InflictDamageToSelected(int damage)
 		{
@@ -19,6 +24,23 @@
 				{
 					attackable.TakeDamage(damage);
 				}
+			}
+		}
+
+
+		/// <summary>
+		/// The debug pause is a pause where the camera can move.
+		/// </summary>
+		[Cheat]
+		public static void ToggleDebugPause()
+		{
+			_isInDebugPause = !_isInDebugPause;
+
+			Time.timeScale = _isInDebugPause ? 0 : 1;
+
+			if (Camera.main.TryGetComponent(out CameraController cameraController))
+			{
+				cameraController.UseUnscaledDeltaTime = _isInDebugPause;
 			}
 		}
 	}
