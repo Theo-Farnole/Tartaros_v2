3717de07e52a99f79ff87f43f473402619ebee72
TF <theo.farnole@gmail.com>
2021-05-11 12:21:26 +0200

    ADD hover and on click sprite to construction panel

---
+++ Assets/Scripts/Utilities/Components/DisableImageIfSpriteIsNull.cs
@@ -0,0 +1,21 @@
+﻿namespace Tartaros
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[RequireComponent(typeof(Image))]
+	public class DisableImageIfSpriteIsNull : MonoBehaviour
+	{
+		private Image _image = null;
+
+		private void Awake()
+		{
+			_image = GetComponent<Image>();
+		}
+
+		private void Update()
+		{
+			_image.color = _image.overrideSprite != null ? Color.white : Color.clear;
+		}
+	}
+}
