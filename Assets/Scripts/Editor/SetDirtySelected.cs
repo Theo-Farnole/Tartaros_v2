ca118ae2ed278ba984fa3c6479a54d2d5646dadd
TF <theo.farnole@gmail.com>
2021-05-11 14:42:26 +0200

    ADD set dirty tool

---
+++ Assets/Scripts/Editor/SetDirtySelected.cs
@@ -0,0 +1,18 @@
+﻿namespace Tartaros.Editor
+{
+	using UnityEditor;
+
+	public static class SetDirtySelected
+	{
+		[MenuItem("Set dirty selected objects than save.")]
+		public static void SetDirtSelected()
+		{
+			foreach (UnityEngine.Object obj in Selection.objects)
+			{
+				EditorUtility.SetDirty(obj);
+			}
+
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
