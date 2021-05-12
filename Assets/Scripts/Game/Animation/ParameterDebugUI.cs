e4115454e3ef8a555f3a9becfe8a3a09e4f581a9
TF <theo.farnole@gmail.com>
2021-05-12 15:01:20 +0200

    FIX missing characters in build + ADD debug tools: set parameter of animator in runtime + FIX GPUAnimator synchronization failing with Animator component

---
+++ Assets/Scripts/Game/Animation/ParameterDebugUI.cs
@@ -0,0 +1,73 @@
+﻿namespace Tartaros.Animation
+{
+	using UnityEngine;
+
+	public class ParameterDebugUI : MonoBehaviour
+	{
+		public const int BUTTON_SIZE = 60;
+		public const int TEXT_FIELD_SIZE = 180;
+
+		[SerializeField] private Animator[] _animators = new Animator[0];
+
+		[SerializeField] private string _booleanParameterName = string.Empty;
+		[SerializeField] private string _triggerParameterName = string.Empty;
+
+
+		private void OnGUI()
+		{
+			GUILayout.BeginVertical(new GUIStyle(GUI.skin.box));
+			{
+				DrawBooleanGUI();
+				DrawTriggerGUI();
+			}
+			GUILayout.EndVertical();
+		}
+
+		private void DrawTriggerGUI()
+		{
+			GUILayout.BeginHorizontal();
+			{
+				GUILayout.Label("Trigger");
+				_triggerParameterName = GUILayout.TextField(_triggerParameterName, GUILayout.Width(TEXT_FIELD_SIZE));
+
+				if (GUILayout.Button("Trigger", GUILayout.Width(BUTTON_SIZE)))
+				{
+					foreach (var animator in _animators)
+					{
+						animator.SetTrigger(_triggerParameterName);
+					}
+				}
+			}
+			GUILayout.EndHorizontal();
+		}
+
+		private void DrawBooleanGUI()
+		{
+			GUILayout.BeginHorizontal();
+			{
+				GUILayout.Label("Boolean");
+				_booleanParameterName = GUILayout.TextField(_booleanParameterName, GUILayout.Width(TEXT_FIELD_SIZE));
+
+				if (GUILayout.Button("Active", GUILayout.Width(BUTTON_SIZE)))
+				{
+					SetBool(_booleanParameterName, true);
+				}
+
+				if (GUILayout.Button("Disable", GUILayout.Width(BUTTON_SIZE)))
+				{
+					SetBool(_booleanParameterName, false);
+				}
+			}
+			GUILayout.EndHorizontal();
+		}
+
+		private void SetBool(string name, bool value)
+		{
+			foreach (var animator in _animators)
+			{
+				animator.SetBool(name, value);
+			}
+		}
+	}
+
+}
