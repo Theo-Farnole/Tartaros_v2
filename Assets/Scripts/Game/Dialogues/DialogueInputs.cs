3cfc4e86cac03d8d59c349a328a5d0ca510eb79b
Dimitry <xxhellfire69xx@gmail.com>
2021-05-12 16:27:51 +0200

    ADD Dialogue state wip

---
+++ Assets/Scripts/Game/Dialogues/DialogueInputs.cs
@@ -0,0 +1,37 @@
+﻿namespace Tartaros.Dialogue
+{
+	using System;
+	using System.Collections;
+	using UnityEngine;
+	using UnityEngine.InputSystem;
+	using static UnityEngine.InputSystem.InputAction;
+
+	public class DialogueInputs
+	{
+		private GameInputs _input = null;
+
+		public DialogueInputs()
+		{
+			_input = new GameInputs();
+			_input.Dialogue.Enable();
+		}
+
+		public event Action<CallbackContext> ValidatePerformed
+		{
+			add
+			{
+				_input.Dialogue.NextSpeech.performed += value;
+			}
+
+			remove
+			{
+				_input.Dialogue.NextSpeech.performed -= value;
+			}
+		}
+
+		public bool IsValidatePerformed()
+		{
+			return _input.Dialogue.NextSpeech.phase == InputActionPhase.Performed;
+		}
+	}
+}
--- Assets/Scripts/Game/GameMode/State/DialogueState.cs
+++ Assets/Scripts/Game/GameMode/State/DialogueState.cs
@@ -2,16 +2,22 @@
 {
     using System.Collections;
     using System.Collections.Generic;
-    using UnityEngine;
+	using Tartaros.Dialogue;
+	using UnityEngine;
 
     public class DialogueState : AGameState
     {
-        
-        public DialogueState(GamemodeManager stateOwner) : base(stateOwner)
-        {
 
+        private DialoguesData _data = null;
+        private DialogueInputs _inputs = null;
+        
+        public DialogueState(GamemodeManager stateOwner, DialoguesData data) : base(stateOwner)
+        {
+            _data = data;
+            _inputs = new DialogueInputs();
         }
 
         
+
     }
 }
