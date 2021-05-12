5eace733dde01fa3c5d8a04dc7ad85120734bae9
Dimitry <xxhellfire69xx@gmail.com>
2021-05-12 15:05:40 +0200

    ADD Dialogue database

---
+++ Assets/Scripts/Game/Dialogues/CharacterActorData.cs
@@ -0,0 +1,17 @@
+﻿namespace Tartaros.Dialogue
+{
+	using Sirenix.OdinInspector;
+	using System.Collections;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[System.Serializable]
+	public class CharacterActorData : SerializedScriptableObject
+	{
+		[SerializeField, PreviewField] private Sprite _avatar = null;
+		[SerializeField] private string _name = null;
+
+		public Sprite Avatar => _avatar;
+		public string Name => _name;
+	}
+}
---
+++ Assets/Scripts/Game/Dialogues/DialoguesData.cs
@@ -0,0 +1,13 @@
+﻿namespace Tartaros.Dialogue
+{
+	using Sirenix.OdinInspector;
+	using System.Collections;
+	using UnityEngine;
+
+	public class DialoguesData : SerializedScriptableObject
+	{
+		[SerializeField] private DialogueSequence[] _dialogues = null;
+
+		public DialogueSequence[] Dialogues => _dialogues;
+	}
+}
