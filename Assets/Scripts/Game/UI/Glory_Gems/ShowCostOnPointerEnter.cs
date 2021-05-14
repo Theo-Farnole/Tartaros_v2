590d3ea7f031abeaecb918bfd259aea7c887e530
TF <theo.farnole@gmail.com>
2021-05-14 16:06:54 +0200

    ADD show cost on pointer enter

---
+++ Assets/Scripts/Game/UI/Glory_Gems/ShowCostOnPointerEnter.cs
@@ -0,0 +1,25 @@
+﻿namespace Tartaros.UI
+{
+	using UnityEngine;
+	using UnityEngine.EventSystems;
+
+	public class ShowCostOnPointerEnter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	{
+		#region Fields
+		[SerializeField] private GloryGemsManagerUI _gloryGemsManagerUI = null;
+		[SerializeField] private int _gloryCost = 1;
+		#endregion Fields
+
+		#region Methods
+		void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+		{
+			_gloryGemsManagerUI.ShowCostPreview(_gloryCost);
+		}
+
+		void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+		{
+			_gloryGemsManagerUI.HideCostPreview();
+		}
+		#endregion Methods
+	}
+}
