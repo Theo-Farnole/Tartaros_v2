33c7e950ffb2fea869f14cff8a65f71f547ba68f
TF <theo.farnole@gmail.com>
2021-05-18 15:54:23 +0200

    ADD remove last occurence extension method + its units tests

---
+++ Assets/Scripts/Editor/Collections/Test_RemoveLastOccurenceElement.cs
@@ -0,0 +1,115 @@
+﻿namespace Tartaros.Tests.Collections
+{
+	using NUnit.Framework;
+	using System.Collections.Generic;
+
+	public class Test_RemoveLastOccurenceElement
+	{
+		[Test]
+		public void When_NoOccurence_Should_DoNotTouchTheQueue()
+		{
+			Queue<int> expectedQueue = ConstructQueueWithThreeZero();
+			Queue<int> actualQueue = new Queue<int>(expectedQueue);
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(1);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+
+		[Test]
+		public void When_OnlyOccurences_Should_RemoveLastElement()
+		{
+			Queue<int> expectedQueue = ConstructQueueWithThreeZero();
+			expectedQueue.Dequeue();
+
+			Queue<int> actualQueue = new Queue<int>(ConstructQueueWithThreeZero());
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(0);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+
+		[Test]
+		public void When_OneOccurenceAtTheStart_Should_RemoveIt()
+		{
+			Queue<int> expectedQueue = new Queue<int>();
+			expectedQueue.Enqueue(1);
+			expectedQueue.Enqueue(1);
+
+			Queue<int> actualQueue = new Queue<int>();
+			actualQueue.Enqueue(0);
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(1);
+
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(0);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+
+		[Test]
+		public void When_OneOccurenceAtTheEnd_Should_RemoveIt()
+		{
+			Queue<int> expectedQueue = new Queue<int>();
+			expectedQueue.Enqueue(1);
+			expectedQueue.Enqueue(1);
+
+			Queue<int> actualQueue = new Queue<int>();
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(0);
+
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(0);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+
+		[Test]
+		public void When_OneOccurenceInTheMiddle_Should_RemoveIt()
+		{
+			Queue<int> expectedQueue = new Queue<int>();
+			expectedQueue.Enqueue(1);
+			expectedQueue.Enqueue(1);
+
+			Queue<int> actualQueue = new Queue<int>();
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(0);
+			actualQueue.Enqueue(1);
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(0);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+		
+		[Test]
+		public void When_TwoOccurence_Should_RemoveLast()
+		{
+			Queue<int> actualQueue = new Queue<int>(); // 01101
+			actualQueue.Enqueue(0);
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(1);
+			actualQueue.Enqueue(0);
+			actualQueue.Enqueue(1);
+
+			Queue<int> expectedQueue = new Queue<int>(); // 0111
+			expectedQueue.Enqueue(0);
+			expectedQueue.Enqueue(1);
+			expectedQueue.Enqueue(1);
+			expectedQueue.Enqueue(1);
+
+			actualQueue = actualQueue.RemoveLastOccurenceOf(0);
+
+			Assert.AreEqual(expectedQueue, actualQueue);
+		}
+
+		private static Queue<int> ConstructQueueWithThreeZero()
+		{
+			Queue<int> expectedQueue = new Queue<int>();
+			expectedQueue.Enqueue(0);
+			expectedQueue.Enqueue(0);
+			expectedQueue.Enqueue(0);
+			return expectedQueue;
+		}
+	}
+}
--- Assets/Scripts/Utilities/Extensions/CollectionExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/CollectionExtensions.cs
@@ -2,6 +2,8 @@
 {
 	using System.Collections;
 	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEditor;
 
 	public static class CollectionExtensions
 	{
@@ -25,6 +27,37 @@
 		public static bool IsEmpty<T>(this T[] array)
 		{
 			return array.Length == 0;
+		}
+
+		public static Queue<T> RemoveLastOccurenceOf<T>(this Queue<T> queue, T element)
+		{
+			List<T> list = queue.ToList();
+			list.Reverse();
+
+			// browse it until first occurence + note its index
+			int lastOccurenceIndex = -1;
+
+			for (int i = 0, length = list.Count; i < length; i++)
+			{
+				if (list[i].Equals(element))
+				{
+					lastOccurenceIndex = i;
+					break;
+				}
+			}
+
+			// occurence found
+			if (lastOccurenceIndex >= 0)
+			{
+				list.RemoveAt(lastOccurenceIndex);
+				list.Reverse();
+
+				return new Queue<T>(list);
+			}
+			else
+			{
+				return new Queue<T>(queue);
+			}
 		}
 	}
 }
