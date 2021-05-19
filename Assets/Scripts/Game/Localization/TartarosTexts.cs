9cb506b6e2d242474a1893d40138b76fe6090c0e
TF <theo.farnole@gmail.com>
2021-05-19 10:44:24 +0200

    ADD color units and resources words in UI description (🎉 it's the thousandth commit)

--- Assets/Scripts/Game/Localization/TartarosTexts.cs
+++ Assets/Scripts/Game/Localization/TartarosTexts.cs
@@ -1,14 +1,22 @@
 ﻿namespace Tartaros
 {
+	using Sirenix.Utilities;
+	using Tartaros.Economy;
 	using Tartaros.Map;
+	using UnityEngine;
 
 	public static class TartarosTexts
 	{
-		public static string CAPTURE_SECTOR_FREE = "FREE";
-		public static string CAPTURE_SECTOR = "Capture";
-		public static string SECTOR_CAPTURED = "Sector captured";
-		public static string FAVOR = "favor";
-		public static string CONSTRUCT = "Construct";
+		public static readonly string CAPTURE_SECTOR_FREE = "FREE";
+		public static readonly string CAPTURE_SECTOR = "Capture";
+		public static readonly string SECTOR_CAPTURED = "Sector captured";
+		public static readonly string FAVOR = "favor";
+		public static readonly string CONSTRUCT = "Construct";
+		public static string TEMPLE_DESCRIPTION => string.Format("Train <color={0}>hoplites</color> and <color={0}>archers</color> to defend your people.", UNITS_COLOR_HEX);
+		public static readonly string TEMPLE_NAME = "Temple";
+
+		private static readonly string RESOURCE_COLOR_HEX = Color.green.ToHex();
+		private static readonly string UNITS_COLOR_HEX = Color.red.ToHex();
 
 
 		public static string GetResourceSectorName(ISector sector)
@@ -18,7 +26,8 @@
 
 		public static string GetResourceSectorDescription(ISector sector)
 		{
-			return string.Format("Generates {0}.", sector.GetResourceType());
+			SectorRessourceType resourceType = sector.GetResourceType();
+			return string.Format("Generates {0}<color={1}>{2}</color>.", resourceType.GetRichTextSprite(), RESOURCE_COLOR_HEX, resourceType);
 		}
 	}
 }
---
+++ Assets/Scripts/Utilities/Extensions/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Tartaros
+{
+	using UnityEngine;
+
+	public static class ColorExtensions
+	{
+		/// <returns># + the color to hex</returns>
+		public static string ToHex(this Color c)
+		{
+			return "#{0}".Format(ColorUtility.ToHtmlStringRGB(c));
+		}
+	}
+}
