fb4ad6fe009c6036a1b9956bf565421b57fe8c2b
TF <theo.farnole@gmail.com>
2021-05-28 11:33:50 +0200

    ADD enable gpu instancing on materials

--- Assets/Scripts/Editor/OptimizationWindow.cs
+++ Assets/Scripts/Editor/OptimizationWindow.cs
@@ -10,7 +10,7 @@
 		private Material[] _materials = null;
 
 		private Vector2 _scrollPosition = default;
-		private int _fixedMaterials = 0;
+		private int _fixedMaterials = -1;
 
 		[MenuItem("Tartaros/Open optimization window", priority = 500)]
 		public static void ShowWindow()
@@ -23,8 +23,6 @@
 		{
 			SirenixEditorGUI.Title("GPU Instancing", "", TextAlignment.Left, true);
 
-			GUILayout.Label("{0} materials fixed.".Format(_fixedMaterials));
-
 			if (_materials == null || _materials.Length == 0)
 			{
 				if (_materials != null && _materials.Length == 0)
@@ -36,10 +34,17 @@
 				{
 					_materials = FindMaterialsWithDisableInstancing();
 				}
+
+				if (_fixedMaterials != -1)
+				{
+					GUILayout.Label("{0} materials fixed.".Format(_fixedMaterials));
+				}
 			}
 			else
 			{
 				DrawMaterials();
+
+				GUILayout.Label("{0} materials to fix.".Format(_materials.Length));
 
 				if (GUILayout.Button("Enable GPU instancing on materials"))
 				{
@@ -78,7 +83,7 @@
 
 		private Material[] FindMaterialsWithDisableInstancing()
 		{
-			Material[] materials = AssetDatabase.FindAssets("t:material", new string[] { "Assets/Arts" })
+			Material[] materials = AssetDatabase.FindAssets("t:material", new string[] { "Assets/Arts", "Assets/Scenes" })
 				.Select(guid => AssetDatabase.GUIDToAssetPath(guid))
 				.Select(path => AssetDatabase.LoadAssetAtPath<Material>(path))
 				.ToArray();
