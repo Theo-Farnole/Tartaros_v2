defd708d4d20fdd061c4e3752b9ab28457e99dd5
TF <theo.farnole@gmail.com>
2021-05-28 14:23:51 +0200

    ADD unit test about overlapping convex polygon

--- Assets/Scripts/Units_Tests/Editor_Tests/Maths/OverlapTests_CircleWithConvexPolygon.cs
+++ Assets/Scripts/Units_Tests/Editor_Tests/Maths/OverlapTests_CircleWithConvexPolygon.cs
@@ -1,10 +1,10 @@
-﻿namespace Tartaros.Tests
+﻿namespace Tartaros.Tests.Collisions
 {
 	using NUnit.Framework;
 	using Tartaros.Math;
 	using UnityEngine;
 
-	public class OverlapTests_CircleWithPolygon
+	public class OverlapTests_CircleWithConvexPolygon
 	{
 		[Test]
 		public void When_OverlapCenter_Should_ReturnTrue()
@@ -31,6 +31,22 @@
 			Circle circle = new Circle(new Vector2(5, 1), 1);
 
 			Assert.IsFalse(CollisionOverlapCalculator.DoOverlap(polygon, circle));
+		}
+
+		[Test]
+		public void When_OverlapNonConvex_Should_ReturnFalse()
+		{
+			ConvexPolygon polygon = new ConvexPolygon(
+				Vector2.one,
+				new Vector2(2, 0),
+				new Vector2(2, 2),
+				new Vector2(1, 1),
+				new Vector2(3, 0)
+			);
+
+			Circle circle = new Circle(new Vector2(1, 1.5f), 0.1f);
+
+			Assert.IsTrue(CollisionOverlapCalculator.DoOverlap(polygon, circle));
 		}
 
 		private static ConvexPolygon CreateSquarePolygon()
