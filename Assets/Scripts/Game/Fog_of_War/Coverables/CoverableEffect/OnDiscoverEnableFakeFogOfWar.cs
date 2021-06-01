dc3152e1cc1f45e3d0a61f2b08326f368f8a3921
TF <theo.farnole@gmail.com>
2021-06-01 12:43:39 +0200

    FIX tree was enabling fake projection while becoming covering whereas they should keep disabled fake projection

--- Assets/Scripts/Game/Fog_of_War/Coverables/CoverableEffect/OnDiscoverEnableFakeFogOfWar.cs
+++ Assets/Scripts/Game/Fog_of_War/Coverables/CoverableEffect/OnDiscoverEnableFakeFogOfWar.cs
@@ -3,23 +3,29 @@
 	using Sirenix.OdinInspector;
 	using UnityEngine;
 
-	public class OnCoverEnableFakeFogOfWar : MonoBehaviour, ICoverableEffect
+	public class OnDiscoverEnableFakeFogOfWar : MonoBehaviour, ICoverableEffect
 	{
 		#region Fields
 		public static readonly int ENABLE_FAKE_FOG_OF_WAR_PROJECTION = Shader.PropertyToID("_EnableFakeFoWProjection");
 
 		[SerializeField, Required] private MeshRenderer _meshRenderer = null;
+
+		private bool _hasBeenVisible = false;
 		#endregion Fields
 
 		#region Methods
 		void ICoverableEffect.OnBecomeCover()
 		{
-			EnableFakeProjection(true);
+			if (_hasBeenVisible == false)
+			{
+				EnableFakeProjection(true);
+			}
 		}
 
 		void ICoverableEffect.OnBecomeVisible()
 		{
 			EnableFakeProjection(false);
+			_hasBeenVisible = true;
 		}
 
 		private void EnableFakeProjection(bool enable)
