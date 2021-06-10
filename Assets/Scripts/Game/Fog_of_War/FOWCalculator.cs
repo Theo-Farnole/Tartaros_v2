730ead1a5033e8d99f648471bea01a02ff52b25a
TF <theo.farnole@gmail.com>
2021-06-10 10:35:20 +0200

    RETAKE move Job into FOWCalculator.cs

--- Assets/Scripts/Game/Fog_of_War/FOWCalculator.cs
+++ Assets/Scripts/Game/Fog_of_War/FOWCalculator.cs
@@ -3,15 +3,108 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using Tartaros.Math;
+	using Unity.Burst;
 	using Unity.Collections;
 	using Unity.Jobs;
 	using UnityEngine;
+
+	[BurstCompile]
+	struct CalculateFogVisibilityJob : IJobParallelFor
+	{
+		[ReadOnly] public NativeArray<Circle> visionsCircle;
+		[ReadOnly] public NativeArray<Vector2> coverables;
+
+		[ReadOnly] public int polygonCount;
+		[ReadOnly] public NativeList<Vector2> visionsPolygon;
+		[ReadOnly] public NativeArray<int> visionsPolygonStartIndexes;
+		[ReadOnly] public NativeArray<int> visionsPolygonEndIndexes;
+
+		public NativeArray<bool> isCoverResult;
+
+		void IJobParallelFor.Execute(int index)
+		{
+			Vector2 coverablePoint = coverables[index];
+
+			if (CheckCircleCollisions(coverablePoint) == true || CheckPolygonCollisions(coverablePoint) == true)
+			{
+				isCoverResult[index] = false;
+			}
+			else
+			{
+				isCoverResult[index] = true;
+			}
+		}
+
+		private bool CheckCircleCollisions(Vector2 coverablePoint)
+		{
+			for (int i = 0, length = visionsCircle.Length; i < length; i++)
+			{
+				bool isVisible = CollisionOverlapCalculator.DoPointOverlapCircle(coverablePoint.x, coverablePoint.y, visionsCircle[i].position.x, visionsCircle[i].position.y, visionsCircle[i].radius);
+
+				if (isVisible == true)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private bool CheckPolygonCollisions(Vector2 coverablePoint)
+		{
+			for (int i = 0, length = polygonCount; i < length; i++)
+			{
+				NativeSlice<Vector2> vertices = new NativeSlice<Vector2>(visionsPolygon, visionsPolygonStartIndexes[i], visionsPolygonEndIndexes[i]);
+
+				bool isVisible = DoPolygonOverlapPoint(vertices, coverablePoint.x, coverablePoint.y);
+
+				if (isVisible == true)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public bool DoPolygonOverlapPoint(NativeSlice<Vector2> vertices, float px, float py)
+		{
+			bool collision = false;
+
+			// go through each of the vertices, plus the next
+			// vertex in the list
+			int next = 0;
+			for (int current = 0; current < vertices.Length; current++)
+			{
+
+				// get next vertex in list
+				// if we've hit the end, wrap around to 0
+				next = current + 1;
+				if (next == vertices.Length) next = 0;
+
+				// get the PVectors at our current position
+				// this makes our if statement a little cleaner
+				Vector2 vc = vertices[current];    // c for "current"
+				Vector2 vn = vertices[next];       // n for "next"
+
+				// compare position, flip 'collision' variable
+				// back and forth
+				if (((vc.y > py && vn.y < py) || (vc.y < py && vn.y > py)) &&
+					 (px < (vn.x - vc.x) * (py - vc.y) / (vn.y - vc.y) + vc.x))
+				{
+					collision = !collision;
+				}
+			}
+			return collision;
+		}
+	}
 
 	class FOWCalculator
 	{
 		private NativeArray<Circle> visionsCircle;
 		private NativeArray<Vector2> coverablesPosition;
 		private NativeArray<bool> isCoverResult;
+
 		private NativeList<Vector2> visionsPolygon;
 		private NativeArray<int> visionsPolygonStartIndexes;
 		private NativeArray<int> visionsPolygonEndIndexes;
@@ -20,10 +113,10 @@
 
 		public void Update(List<IFogVision> _visions, List<IFogCoverable> coverables)
 		{
-			IShape[] visions = _visions.Select(x => x.VisionShape).ToArray();
+			IEnumerable<IShape> visions = _visions.Select(x => x.VisionShape);
 
 			Circle[] circles = visions.OfType<Circle>().ToArray();
-			
+
 			visionsCircle = new NativeArray<Circle>(circles, Allocator.TempJob);
 			isCoverResult = new NativeArray<bool>(coverables.Count, Allocator.TempJob);
 
@@ -52,15 +145,15 @@
 				coverables[i].IsCovered = isCoverResult[i];
 			}
 
-			visionsCircle.Dispose();			
-			this.coverablesPosition.Dispose();
+			visionsCircle.Dispose();
+			coverablesPosition.Dispose();
 			isCoverResult.Dispose();
 			visionsPolygon.Dispose();
 			visionsPolygonStartIndexes.Dispose();
 			visionsPolygonEndIndexes.Dispose();
 		}
 
-		private void CreateContainer_PolygonVisions(IShape[] visions)
+		private void CreateContainer_PolygonVisions(IEnumerable<IShape> visions)
 		{
 			ConvexPolygon[] polygons = visions.OfType<ConvexPolygon>().ToArray();
 
@@ -84,7 +177,12 @@
 
 		private void CreateCoverablesPosition(List<IFogCoverable> coverables)
 		{
-			Vector2[] coverablesPosition = coverables.Select(x => x.ModelBounds).OfType<Circle>().Select(x => x.position).ToArray();
+			Vector2[] coverablesPosition = coverables
+				.Select(x => x.ModelBounds)
+				.OfType<Circle>()
+				.Select(x => x.position)
+				.ToArray();
+
 			this.coverablesPosition = new NativeArray<Vector2>(coverablesPosition, Allocator.TempJob);
 		}
 	}
--- Assets/Scripts/Game/Fog_of_War/FogOfWarManager.cs
+++ Assets/Scripts/Game/Fog_of_War/FogOfWarManager.cs
@@ -1,16 +1,9 @@
 ﻿namespace Tartaros.FogOfWar
 {
-	using Sirenix.Utilities;
-	using System.CodeDom.Compiler;
 	using System.Collections.Generic;
 	using System.Linq;
 	using Tartaros.Math;
-	using Tartaros.ServicesLocator;
-	using Unity.Burst;
-	using Unity.Collections;
-	using Unity.Jobs;
 	using UnityEngine;
-	using UnityEngine.ParticleSystemJobs;
 
 	public class FogOfWarManager : MonoBehaviour
 	{
@@ -103,96 +96,5 @@
 			}
 		}
 		#endregion Methods
-	}
-
-	[BurstCompile]
-	struct CalculateFogVisibilityJob : IJobParallelFor
-	{
-		[ReadOnly] public NativeArray<Circle> visionsCircle;
-		[ReadOnly] public NativeArray<Vector2> coverables;
-
-		[ReadOnly] public int polygonCount;
-		[ReadOnly] public NativeList<Vector2> visionsPolygon;
-		[ReadOnly] public NativeArray<int> visionsPolygonStartIndexes;
-		[ReadOnly] public NativeArray<int> visionsPolygonEndIndexes;
-
-		public NativeArray<bool> isCoverResult;
-
-		void IJobParallelFor.Execute(int index)
-		{
-			Vector2 coverablePoint = coverables[index];
-
-			if (CheckCircleCollisions(coverablePoint) == true || CheckPolygonCollisions(coverablePoint) == true)
-			{
-				isCoverResult[index] = false;
-			}
-			else
-			{
-				isCoverResult[index] = true;
-			}
-		}
-
-		private bool CheckCircleCollisions(Vector2 coverablePoint)
-		{
-			for (int i = 0, length = visionsCircle.Length; i < length; i++)
-			{
-				bool isVisible = CollisionOverlapCalculator.DoPointOverlapCircle(coverablePoint.x, coverablePoint.y, visionsCircle[i].position.x, visionsCircle[i].position.y, visionsCircle[i].radius);
-
-				if (isVisible == true)
-				{
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		private bool CheckPolygonCollisions(Vector2 coverablePoint)
-		{
-			for (int i = 0, length = polygonCount; i < length; i++)
-			{
-				NativeSlice<Vector2> vertices = new NativeSlice<Vector2>(visionsPolygon, visionsPolygonStartIndexes[i], visionsPolygonEndIndexes[i]);
-
-				bool isVisible = DoPolygonOverlapPoint(vertices, coverablePoint.x, coverablePoint.y);
-
-				if (isVisible == true)
-				{
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		public bool DoPolygonOverlapPoint(NativeSlice<Vector2> vertices, float px, float py)
-		{
-			bool collision = false;
-
-			// go through each of the vertices, plus the next
-			// vertex in the list
-			int next = 0;
-			for (int current = 0; current < vertices.Length; current++)
-			{
-
-				// get next vertex in list
-				// if we've hit the end, wrap around to 0
-				next = current + 1;
-				if (next == vertices.Length) next = 0;
-
-				// get the PVectors at our current position
-				// this makes our if statement a little cleaner
-				Vector2 vc = vertices[current];    // c for "current"
-				Vector2 vn = vertices[next];       // n for "next"
-
-				// compare position, flip 'collision' variable
-				// back and forth
-				if (((vc.y > py && vn.y < py) || (vc.y < py && vn.y > py)) &&
-					 (px < (vn.x - vc.x) * (py - vc.y) / (vn.y - vc.y) + vc.x))
-				{
-					collision = !collision;
-				}
-			}
-			return collision;
-		}
 	}
 }
