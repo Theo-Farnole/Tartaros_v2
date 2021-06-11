e572390c82999e7960b674117e9c101d6754b905
TF <theo.farnole@gmail.com>
2021-06-11 14:48:46 +0200

    ADD main menu + ADD play button

---
+++ Assets/Scripts/Game/UI/Mini-Map/PlayButton.cs
@@ -0,0 +1,36 @@
+﻿namespace Tartaros.UI.MainMenu
+{
+	using System;
+	using Tartaros.UI;
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public class PlayButton : AButtonActionAttacher
+	{
+		[SerializeField] private SceneReference _sceneToLoad = null;
+		[SerializeField] private bool _loadAsyncOnStart = true;
+
+		private AsyncOperation _asyncOperation = null;
+
+		private void Start()
+		{
+			if (_loadAsyncOnStart == true)
+			{
+				_asyncOperation = SceneManager.LoadSceneAsync(_sceneToLoad.ScenePath);
+				_asyncOperation.allowSceneActivation = false;
+			}
+		}
+
+		protected override void OnButtonClick()
+		{
+			if (_asyncOperation != null)
+			{
+				_asyncOperation.allowSceneActivation = true;
+			}
+			else
+			{
+				SceneManager.LoadScene(_sceneToLoad);
+			}
+		}
+	}
+}
