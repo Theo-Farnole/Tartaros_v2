c87cd5bc88466e997756dd19edc8491f6d9ed2a2
TF <theo.farnole@gmail.com>
2021-06-11 14:16:56 +0200

    ADD set dirty databases menu items

--- Assets/Scripts/Editor/SetDirtySelected.cs
+++ Assets/Scripts/Editor/SetDirtySelected.cs
@@ -1,10 +1,12 @@
 ﻿namespace Tartaros.Editor
 {
+	using System.Linq;
 	using UnityEditor;
+	using UnityEngine;
 
 	public static class SetDirtySelected
 	{
-		[MenuItem("Tartaros/Set dirty selected objects than save.")]
+		[MenuItem("Tartaros/Dirty/selected objects")]
 		public static void SetDirtSelected()
 		{
 			foreach (UnityEngine.Object obj in Selection.objects)
@@ -13,6 +15,26 @@
 			}
 
 			AssetDatabase.SaveAssets();
+		}
+
+		[MenuItem("Tartaros/Dirty/databases")]
+		public static void SetDirtyDatabases()
+		{
+			var scriptablesObjects = AssetDatabase.FindAssets("t:ScriptableObject", new string[] { "Assets/Databases" })
+				.Select(guid => AssetDatabase.GUIDToAssetPath(guid))
+				.Select(path => AssetDatabase.LoadAssetAtPath<ScriptableObject>(path))
+				.ToArray();
+
+			Debug.LogFormat("Starting set dirty... {0} elements to process", scriptablesObjects.Length);
+
+			foreach (ScriptableObject scriptableObject in scriptablesObjects)
+			{
+				EditorUtility.SetDirty(scriptableObject);
+			}
+
+			AssetDatabase.SaveAssets();
+
+			Debug.LogFormat("Set dirty done. {0} databases processed.", scriptablesObjects.Length);
 		}
 	}
 }
