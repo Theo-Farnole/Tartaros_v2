76043cfdc09bdb1b506a11f8cd36e8c7c823ddf4
TF <theo.farnole@gmail.com>
2021-06-17 10:17:55 +0200

    ADD launch dialog at start

---
+++ Assets/Scripts/Game/Dialogues/LaunchDialogAtStart.cs
@@ -0,0 +1,22 @@
+﻿namespace Tartaros.Dialogue
+{
+	using Tartaros.ServicesLocator;
+	using UnityEngine;
+
+	public class LaunchDialogAtStart : MonoBehaviour
+	{
+		[SerializeField] private string _dialogueID = "";
+
+		private DialogueManager _dialogueManager = null;
+
+		private void Awake()
+		{
+			_dialogueManager = Services.Instance.Get<DialogueManager>();
+		}
+
+		private void Start()
+		{
+			_dialogueManager.EnterDialogueState(_dialogueID);
+		}
+	}
+}
